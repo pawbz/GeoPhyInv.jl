e6e33fbe30cba43eee9dd73272f125cf55240157
Pawan Bharadwaj <bharadwaj.pawan@gmail.com>
2017-02-08 12:37:54 -0500

    added f90libs.jl

--- src/SeismicInversion.jl
+++ src/SeismicInversion.jl
@@ -1,5 +1,7 @@
 module SeismicInversion
 
 # package code goes here
+include("f90libs.jl")
 
+f90libs.say_hello()
 end # module
---
+++ src/f90libs.jl
@@ -0,0 +1,13 @@
+
+module f90libs
+
+function say_hello()
+ println("HElo")
+end # function
+
+export say_hello
+
+say_hello()
+
+end # module 
+
