40c31dc6efd75ebab2dab3a118a22524df917b24
Pawan Bharadwaj <bharadwaj.pawan@gmail.com>
2017-02-13 22:35:52 -0500

    successfully wrapped fortran90 subroutines in test.f90

--- srcf90/test.f90
+++ srcf90/test.f90
@@ -1,16 +1,13 @@
 module test
 use precision_mod! USEINDEPFILE
 
-        type, bind(c) :: model
-                real            :: m1, m2, m3
-        endtype
 contains
 
 ! subroutine simple print 
 subroutine sprint() bind(c, name="sprint")
         implicit none
 
-        write(*,*) "sprint success in test.f90"
+        write(*,*) repeat(" ",10),"sprint success in test.f90"
 
 end subroutine sprint
 
@@ -21,24 +18,32 @@ subroutine sprint_core() bind(c, name="sprint_core")
         implicit none
 
         !$OMP PARALLEL
-        write(*,*) "sprint success core", OMP_GET_THREAD_NUM(), "in test.f90"
+        write(*,*) repeat(" ",10),"sprint success core", OMP_GET_THREAD_NUM(), "in test.f90"
         !$OMP END PARALLEL
 
 end subroutine sprint_core
 
-subroutine matrix_in_out(A, B) bind(c, name="matrix_in_out")
+subroutine matrix_in_out(A, B, n1, n2) bind(c, name="matrix_in_out")
         implicit  none
-        real(r_dp), intent(in)           :: A(:)
-        real(r_dp), intent(inout)           :: B(:)
+        real(r_dp), intent(in)              :: A(n1,n2)
+        real(r_dp), intent(inout)           :: B(n1)
+        integer(i_sp), intent(in)           :: n1, n2
 
-        write(*,*) "in fortran ", size(A)
+        write(*,*) repeat(" ",10),"in fortran n1 n2", n1, n2
+        write(*,*) repeat(" ",10),"in fortran A", size(A)
+        write(*,*) repeat(" ",10),"in fortran A", A
+        write(*,*) repeat(" ",10),"in fortran B", size(B)
+        write(*,*) repeat(" ",10),"in fortran B", B
+        B(1) = 0.0
 
 end subroutine matrix_in_out
 
-subroutine derived_data_type(model_in, model_out) bind(c, name="derived_data_type")
-        implicit none
-        type(model), intent(in)         :: model_in
-        type(model), intent(out)        :: model_out
-end subroutine derived_data_type
+!subroutine derived_data_type(model_in, model_out) bind(c, name="derived_data_type")
+!        implicit none
+!        type(model), intent(in)         :: model_in
+!        type(model), intent(inout)        :: model_out
+!        write(*,*) repeat(" ",10),"in fortran model%A", model_in%A
+!        write(*,*) repeat(" ",10),"in fortran model%B", model_in%B
+!end subroutine derived_data_type
 
 end module test
--- test/f90tests.jl
+++ test/f90tests.jl
@@ -3,23 +3,36 @@ using SeismicInversion
 println("testing Julia wrappers for f90 libraries...")
 
 # simple print
+println(repeat(" ",5), "simple print...")
 ccall( (:sprint, SeismicInversion.f90libs.test), Void, () )
 
 # simple print using OMP
+println(repeat(" ",5), "simple print using OMP...")
 ccall( (:sprint_core, SeismicInversion.f90libs.test), Void, () )
 
 
 # simple matrix in out
-A = rand(4);
-B = rand(4);
-println(typeof(A))
+println(repeat(" ",5), "simple vectors in and out..")
+n1 = 4; n2 = 2;
+A = rand(n1, n2);
+B = rand(n1);
 
-println("in julia A is ", A)
-ccall( (:matrix_in_out, SeismicInversion.f90libs.test), Void, (Ptr{Array{Float64,1}}, Ptr{Float64}), A, B )
-println("in julia B is ", B)
+println(repeat(" ",10), "in julia A is ", A)
+println(repeat(" ",10), "in julia input B is ", B)
+ccall( (:matrix_in_out, SeismicInversion.f90libs.test), Void, (Ptr{Float64}, Ptr{Float64}, Ref{Int64}, 
+							       Ref{Int64}), A, B, n1, n2)
+println(repeat(" ",10), "in julia output B is ", B)
+
+
 
 # derived data types
-#type model
-
-
-#end #type
+#=
+n1=4; n2=2;
+type Model
+	A::Vector{Float64}
+end #type
+model_in = Model([3,4,5])
+ccall( (:matrix_in_out, SeismicInversion.f90libs.test), Void, (Ptr{Float64}, Ptr{Float64}, Ref{Int64}, 
+							       Ref{Int64}), A, B, n1, n2)
+							       =#
+							       
