923d7511b58c556f90145eb6e94ebad0891748c5
Pawan Bharadwaj <bharadwaj.pawan@gmail.com>
2017-03-12 18:31:55 -0400

    overlapping banded ICA

--- src/Banded_ICA.jl
+++ src/Banded_ICA.jl
@@ -12,35 +12,70 @@ function bica(;
 	X::Array{Float64}=zeros(grid.nx,recv_n) # data 
             	)
 
-nb = (mod(grid.nx, nband) == 0) ? div(grid.nx, nband) : error("change nsamples")
+nb = (mod(grid.nx, nband) == 0) ? div(grid.nx, nband) : error("change nsamples");
+nbh = Int64(floor(nb / 2));
 
 # initialize
-Yall = zeros(src_n,nb,nband);
 Yband = zeros(src_n,nb)
-Qref = zeros(src_n, recv_n)
+Xband = zeros(recv_n,nb,2*nband-1)
+Mband = Array(ICA, 2*nband-1)
+
 # divide into bands and do ICA in each band
-for iband = 1:nband
+for iband = 1: 2 * nband - 1
+	# indices for bands
+	ixmin = (div(iband,2))   * nb - (1-mod(iband,2)) * nbh + 1;
+	ixmax = (div(iband,2)+1) * nb - (1-mod(iband,2)) * nbh;
+	println("fffffff\t", ixmin, "\t", ixmax)
+
 	# window out the data in the band
-	Xband = transpose(X[(iband-1)*nb+1:iband*nb,:]);
-	Mband = fit(ICA, Xband, src_n; 
+	Xband[:,:,iband] = transpose(X[ixmin:ixmax,:]);
+
+	# do ICA
+	Mband[iband] = fit(ICA, Xband[:,:,iband], src_n; 
 	     do_whiten=true, fun=icagfun(:gaus), verbose=false, maxiter=50,
 	     tol=1e-20, winit=ones(src_n,src_n))
 
 	# calculate source signals temporarily
-	Yband = transform(Mband,Xband);
+	Yband = transform(Mband[iband],Xband[:,:,iband]);
 
 	# fix permutation problem
-	Mband.W = kurtosis(Yband[1,:]) < kurtosis(Yband[2,:]) ? Mband.W : Mband.W[:,2:-1:1];
-	
-	# save W during the first band as reference Q
-	if(isequal(iband,1)); Qref = transpose(Mband.W); end;
+	Mband[iband].W = entropy(Yband[1,:]) < entropy(Yband[2,:]) ? 
+			Mband[iband].W : Mband[iband].W[:,2:-1:1];
+end
 
-	# fix scaling problem using Qref
-	Mband.W = Mband.W * diagm(diag((Qref * inv(transpose(Mband.W)))));
+Yall = zeros(src_n,grid.nx);
+Ybandprev = zeros(src_n,nb);
+Yband = zeros(src_n,nb)
+for iband = 1: 2 * nband - 1 
+	# indices
+	ixmin = (div(iband,2))   * nb - (1-mod(iband,2)) * nbh + 1;
+	ixmax = (div(iband,2)+1) * nb - (1-mod(iband,2)) * nbh;
+
+	# temp 
+	Yband = transform(Mband[iband],Xband[:,:,iband]); 
+
+	# fix scaling problem using previous band
+	if(iband > 1)
+		λ = [sum(Ybandprev[1,nb-nbh:nb]./Yband[1,1:1+nbh])/nbh, 
+			sum(Ybandprev[2,nb-nbh:nb]./Yband[2,1:1+nbh])/nbh]
+		println(λ)
+		Mbandtemp = Mband[iband];
+		Mbandtemp.W *= (diagm(λ));
+	#Mbandtemp.W *= diagm(diag((Mband[iband-1].W * inv(transpose(Mband[iband].W)))));
+	else
+		λ = [1., 1.]
+		Mbandtemp = Mband[iband];
+
+	end
 
 	# final λbandtemp after fixing scaling and permutation
-	Yall[:,:,iband] = (transform(Mband,Xband));
+	Yband = (transform(Mbandtemp,Xband[:,:,iband]));
+	Yall[:,ixmin:ixmax] = Yband;
+	Ybandprev = Yband;
 end
+
+
+
 
 return reshape(Yall, (src_n,grid.nx))
 end
