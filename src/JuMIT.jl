ee4810803c1532e80f08fc8d21197c81443251a3
Pawan Bharadwaj <bharadwaj.pawan@gmail.com>
2017-07-21 16:10:31 -0400

    name change

---
+++ src/JuMIT.jl
@@ -0,0 +1,35 @@
+__precompile__()
+
+module JuMIT
+
+const JT = JuMIT
+const SIT = JuMIT
+
+#const depsfile = joinpath(dirname(@__FILE__), "..", "deps", "deps.jl")
+#if isfile(depsfile)
+#	include(depsfile)
+#else
+#	error("JuMIT not properly installed. Please run Pkg.build(\"JuMIT\") then restart Julia.")
+#end
+
+# include modules (note: due to dependencies, order is important!)
+include("IO.jl")
+include("Grid.jl")
+include("Wavelets.jl")
+include("Interpolation.jl")
+include("DSP.jl")
+include("Smooth.jl")
+include("Models.jl")
+include("Acquisition.jl")
+include("Coupling.jl")
+include("Data.jl")
+include("Interferometry.jl")
+include("Gallery.jl")
+include("Analytic.jl")
+include("Fdtd.jl")
+include("Misfits.jl")
+include("Inversion.jl")
+include("CICA.jl")
+include("Plots.jl")
+
+end # module
