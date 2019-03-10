4f4d22594bc859f20b6749c6cc89f7435b4dc14c
Pawan Bharadwaj <bharadwaj.pawan@gmail.com>
2019-03-09 20:19:35 -0500

    Deployed 7e8283a with MkDocs version: 0.16.3

--- modeling/make.jl
+++ modeling/make.jl
@@ -2,7 +2,8 @@
 using Literate
 
 freadme=joinpath(@__DIR__, "page1.jl")
-output_folder=joinpath(@__DIR__, "..","..","..","notebooks","modeling") 
+#output_folder=joinpath(@__DIR__, "..","..","..","notebooks","modeling") 
+output_folder=joinpath(@__DIR__, ".") 
 
 
 #Literate.notebook(freadme, output_folder)
