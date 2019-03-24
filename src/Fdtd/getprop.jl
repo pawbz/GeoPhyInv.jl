34b9e4418f1d9c6aad744e0ea9400d47ed03ca5f
Pawan Bharadwaj <bharadwaj.pawan@gmail.com>
2019-03-24 19:54:21 -0400

    Fdtd.snaps update

--- src/Fdtd/getprop.jl
+++ src/Fdtd/getprop.jl
@@ -6,9 +6,23 @@ Return snaps stored in `SeisForwExpt` after using `mod!`.
 * `iss::Int64=1` : supersource index
 """
 function Base.getindex(pa::Param, s::Symbol, iss::Int=1)
+	@assert iss≤pa.c.acqgeom[1].nss
 	@assert s in [:snaps]
 	if(s==:snaps)
-		return pa.p.localpart.ss[iss].snaps
+		nzd,nxd=length.(pa.c.model.mgrid)
+		snaps_all=zeros(nzd,nxd,length(pa.c.itsnaps))
+		ip=findall(in.(iss,pa.sschunks))[1]
+		issp=findall(x->x==iss,pa.sschunks[ip])[]
+		@sync begin
+			p=procs(pa.p)[ip]
+			@sync remotecall_wait(p) do 
+				pap=localpart(pa.p)
+				for i in eachindex(snaps_all)
+					snaps_all[i]=pap.ss[issp].snaps[i]
+				end
+			end
+		end
+		return snaps_all
 	end
 end
 
